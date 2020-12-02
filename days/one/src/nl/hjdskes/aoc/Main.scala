271e52534fe12eff4a43843730834a08b69a8f4d
Jente Hidskes <jente.tibor-hidskes@klarna.com>
2020-12-02 20:27:42 +0100

    [Day 1] Add more efficient solution to problem 1

--- days/one/src/nl/hjdskes/aoc/Main.scala
+++ days/one/src/nl/hjdskes/aoc/Main.scala
@@ -5,7 +5,7 @@ import java.nio.file.{Path, Paths}
 import cats.effect.{Blocker, ExitCode, IO, IOApp}
 import fs2.{Stream, io, text}
 
-import scala.annotation.unused
+import scala.annotation.{tailrec, unused}
 
 object Main extends IOApp {
   def readFile(file: Path): Stream[IO, String] = Stream.resource(Blocker[IO]).flatMap { blocker =>
@@ -16,9 +16,22 @@ object Main extends IOApp {
       .filter(_.trim.nonEmpty)
   }
 
+  @tailrec
+  def problemOneEfficiently(l: List[Int]): Option[Int] = l match {
+    case Nil => None
+    case h :: t =>
+      val x = 2020 - h
+      if (t.contains(x)) Some(h * x) else problemOneEfficiently(t)
+  }
+
   def findSum(l: List[Int], n: Int): Option[List[Int]] = l.combinations(n).find(_.sum == 2020)
 
-  def print(solution: Option[List[Int]]): IO[Unit] = solution match {
+  def print1(solution: Option[Int]): IO[Unit] = solution match {
+    case None => IO(println("No solution found"))
+    case Some(solution) => IO(println(s"Solution: $solution"))
+  }
+
+  def print2(solution: Option[List[Int]]): IO[Unit] = solution match {
     case None => IO(println("No solution found"))
     case Some(solution) => IO(println(s"Solution: ${solution.mkString(" + ")} = 2020, product is ${solution.product}"))
   }
@@ -27,7 +40,8 @@ object Main extends IOApp {
     for {
       path <- IO(ClassLoader.getSystemResource("input.txt").toURI).map(Paths.get)
       numbers <- readFile(path).map(_.toInt).compile.toList
-      _ <- print(findSum(numbers, 2))
-      _ <- print(findSum(numbers, 3))
+      _ <- print1(problemOneEfficiently(numbers))
+      _ <- print2(findSum(numbers, 2))
+      _ <- print2(findSum(numbers, 3))
     } yield ExitCode.Success
 }
